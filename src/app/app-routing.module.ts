5ccc750b8de0fe03330990f247cdfc99fe73f195
tungchihuy95 <tungjpvn95@gmail.com>
2021-01-15 16:17:48 +0700

    sign-up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CreateUserComponent} from './create-user/create-user.component';
+// import {UpdateProfileComponent} from './update-profile/update-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CreateUserComponent
+  },
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
---
+++ src/app/model/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+  userId: any;
+  address: any;
+  avatar: any;
+  email: any;
+  fullName: any;
+  password: any;
+  phone: any;
+  username: any;
+}
