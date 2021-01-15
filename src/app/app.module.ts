3f73c01d9ac5c8d1b23de55e8571f127579b8b75
quannv2503 <67447296+quannv2503@users.noreply.github.com>
2021-01-15 22:18:07 +0700

    đầy đủ file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,50 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-// <<<<<<< HEAD
-import { HeaderComponent } from './_shared/header/header.component';
-import { FooterComponent } from './_shared/footer/footer.component';
-import { SearchComponent } from './_shared/search/search.component';
-import { HomeContentComponent } from './modules/home-content/home-content.component';
-import { ListAllHousesComponent } from './modules/page-content/houses/list/list-all-houses/list-all-houses.component';
-import { ListOwnHousesComponent } from './modules/page-content/houses/list/list-own-houses/list-own-houses.component';
-import { AboutContentComponent } from './_shared/about-content/about-content.component';
-import { UserShowComponent } from './modules/page-content/user/detail/user-show/user-show.component';
-import { UserUpdateComponent } from './modules/page-content/user/update/user-update/user-update.component';
-import { HouseDetailPostedComponent } from './modules/page-content/houses/detail/house-detail-posted/house-detail-posted.component';
-import { HouseDetailViewComponent } from './modules/page-content/houses/detail/house-detail-view/house-detail-view.component';
-import { CreateHouseComponent } from './modules/page-content/houses/create/create-house/create-house.component';
-import { PostedHouseEditComponent } from './modules/page-content/houses/edit/posted-house-edit/posted-house-edit.component';
-import { UserChangePasswordComponent } from './modules/page-content/user/update/user-change-password/user-change-password.component';
-import { ListRentedHousesComponent } from './modules/page-content/booking/list/list-rented-houses/list-rented-houses.component';
-import { UserCheckInComponent } from './modules/page-content/booking/edit/user-check-in/user-check-in.component';
-import { BookingHotelComponent } from './modules/page-content/booking/create/booking-hotel/booking-hotel.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './_shared/header/header.component';
+import {FooterComponent} from './_shared/footer/footer.component';
+import {SearchComponent} from './_shared/search/search.component';
+import {HomeContentComponent} from './modules/home-content/home-content.component';
+import {ListAllHousesComponent} from './modules/page-content/houses/list/list-all-houses/list-all-houses.component';
+import {ListOwnHousesComponent} from './modules/page-content/houses/list/list-own-houses/list-own-houses.component';
+import {AboutContentComponent} from './_shared/about-content/about-content.component';
+import {UserShowComponent} from './modules/page-content/user/detail/user-show/user-show.component';
+import {UserUpdateComponent} from './modules/page-content/user/update/user-update/user-update.component';
+import {HouseDetailPostedComponent} from './modules/page-content/houses/detail/house-detail-posted/house-detail-posted.component';
+import {HouseDetailViewComponent} from './modules/page-content/houses/detail/house-detail-view/house-detail-view.component';
+import {CreateHouseComponent} from './modules/page-content/houses/create/create-house/create-house.component';
+import {PostedHouseEditComponent} from './modules/page-content/houses/edit/posted-house-edit/posted-house-edit.component';
+import {UserChangePasswordComponent} from './modules/page-content/user/update/user-change-password/user-change-password.component';
+import {ListRentedHousesComponent} from './modules/page-content/booking/list/list-rented-houses/list-rented-houses.component';
+import {UserCheckInComponent} from './modules/page-content/booking/edit/user-check-in/user-check-in.component';
+import {BookingHotelComponent} from './modules/page-content/booking/create/booking-hotel/booking-hotel.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {LoginComponent} from './modules/authentication/login/login.component';
-// =======
-// import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-// import { UpdateProfileComponent } from './component/user/update-profile/update-profile.component';
-// import { ListUserComponent } from './component/user/list-user/list-user.component';
-// import { UpdatePasswordComponent } from './component/user/update-password/update-password.component';
-// import { ProfileComponent } from './component/user/profile/profile.component';
-// import { LoginComponent } from './component/user/login/login.component';
-// import { HomeComponent } from './component/home/home/home.component';
 import {JwtInterceptor} from './helper/jwt-interceptor';
 import {ErrorInterceptor} from './helper/error-interceptor';
-import { HistoryBookingComponent } from './modules/history-booking/history-booking.component';
-import { StatisticsComponent } from './modules/statistics/statistics.component';
-import { ListRentHousesByTimeComponent } from './modules/list-rent-houses-by-time/list-rent-houses-by-time.component';
-// import {UpdateProfileComponent} from './component/user/update-profile/update-profile.component';
-// import {UpdatePasswordComponent} from './component/user/update-password/update-password.component';
-// >>>>>>> 770c87ac2b1ccf43b5feebfaa58a2a0bc28407b2
+import {HistoryBookingComponent} from './modules/history-booking/history-booking.component';
+import {StatisticsComponent} from './modules/statistics/statistics.component';
+import {ListRentHousesByTimeComponent} from './modules/list-rent-houses-by-time/list-rent-houses-by-time.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-// <<<<<<< HEAD
     HeaderComponent,
     FooterComponent,
     SearchComponent,
@@ -63,17 +50,10 @@ import { ListRentHousesByTimeComponent } from './modules/list-rent-houses-by-tim
     UserCheckInComponent,
     BookingHotelComponent,
     LoginComponent,
-// =======
-//     UpdateProfileComponent,
-    // ListUserComponent,
-    // UpdatePasswordComponent,
-    // ProfileComponent,
     LoginComponent,
-HistoryBookingComponent,
-StatisticsComponent,
-ListRentHousesByTimeComponent,
-    // HomeComponent
-// >>>>>>> 770c87ac2b1ccf43b5feebfaa58a2a0bc28407b2
+    HistoryBookingComponent,
+    StatisticsComponent,
+    ListRentHousesByTimeComponent,
   ],
   imports: [
     BrowserModule,
@@ -82,8 +62,9 @@ ListRentHousesByTimeComponent,
     FormsModule,
     HttpClientModule
   ],
-  providers: [ {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
