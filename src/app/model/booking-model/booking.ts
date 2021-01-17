430d3d108a9f48293ff0c55e2ba5c06b56126d3d
unknown <aloha.hat.2508@gmail.com>
2021-01-17 23:39:34 +0700

    thêm interface booking

--- src/app/model/booking-model/booking.ts
+++ src/app/model/booking-model/booking.ts
@@ -1,2 +1,13 @@
+import {User} from '../user-model/user';
+import {House} from '../house-model/house';
+import {Timestamp} from 'rxjs';
+
 export interface Booking {
+  bookingId?: number;
+  houseId?: House;
+  userId?: User;
+  checkIn?: Date;
+  checkOut?: Date;
+  total?: number;
+  createdAt?: Timestamp<any>;
 }
