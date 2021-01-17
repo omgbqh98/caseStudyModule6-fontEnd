c9e43566bb5b4067f15d6dbf1809687e2a2731b9
unknown <aloha.hat.2508@gmail.com>
2021-01-17 15:13:14 +0700

    Sửa routerlink become a house, thêm path và html

--- src/app/modules/page-content/houses/create/create-house/create-house.component.ts
+++ src/app/modules/page-content/houses/create/create-house/create-house.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {HouseService} from '../../../../../service/house-service/house.service';
+import {Router} from '@angular/router';
+import {House} from '../../../../../model/house-model/house';
+import {UserService} from '../../../../../service/user-service/user.service';
 
 @Component({
   selector: 'app-create-house',
@@ -6,10 +11,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-house.component.css']
 })
 export class CreateHouseComponent implements OnInit {
-
-  constructor() { }
+  // @ts-ignore
+  createHouseForm: FormGroup;
+  constructor(private fb: FormBuilder,
+              private houseService: HouseService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.createHouseForm = this.fb.group({
+      houseName: [''],
+      type: [''],
+      address: [''],
+      description: [''],
+      price: [''],
+      bedroom: [''],
+      bathroom: [''],
+      status: [''],
+    });
+  }
+
+  createHouse() {
+    const house: House = this.createHouseForm.value;
+    this.houseService.create(house).subscribe(() => {
+      alert('Create successfully!');
+    });
   }
 
 }
