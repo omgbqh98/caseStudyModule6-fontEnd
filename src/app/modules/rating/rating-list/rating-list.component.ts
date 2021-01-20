1035fe823c53aaec78e8e24eb425414a490a419d
unknown <aloha.hat.2508@gmail.com>
2021-01-20 11:07:52 +0700

    lấy user hiện tại, check có trong list checkout không trong rating-list ts

--- src/app/modules/rating/rating-list/rating-list.component.ts
+++ src/app/modules/rating/rating-list/rating-list.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import {HouseService} from '../../../service/house-service/house.service';
 import {ActivatedRoute, ParamMap} from '@angular/router';
 import {RatingService} from '../../../service/rating-service/rating.service';
+import {AuthService} from '../../../service/authen-service/auth.service';
+import {UserToken} from '../../../model/user-model/user-token';
+import {User} from '../../../model/user-model/user';
+import {UserService} from '../../../service/user-service/user.service';
 
 @Component({
   selector: 'app-rating-list',
@@ -14,9 +18,14 @@ export class RatingListComponent implements OnInit {
   listParentRating: any;
   listChildRating: any;
   sum: any;
+  currentUser: any;
+  user: any;
+  checkedOutList: Array<User> = [];
+  isShow = false;
   constructor(private houseService: HouseService,
               private activatedRoute: ActivatedRoute,
-              private ratingService: RatingService) {}
+              private ratingService: RatingService,
+              private userService: UserService) {}
 
   ngOnInit(): void {
     this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
@@ -29,6 +38,25 @@ export class RatingListComponent implements OnInit {
         console.log('thành công');
         this.listChildRating = data;
       });
+    });
+  }
+  // tslint:disable-next-line:typedef
+  getCurrentUser() {
+    this.currentUser = JSON.parse(localStorage.getItem('user'));
+    // @ts-ignore
+    console.log(this.currentUser);
+    // @ts-ignore
+    this.userService.getUserProfile(this.currentUser.username).subscribe(value => this.user = value);
+  }
+
+  // tslint:disable-next-line:typedef
+  replyBtnClicked() {
+    this.ratingService.getCheckedOutUserByHouse(this.id).subscribe((users) => {
+      this.getCurrentUser();
+      this.checkedOutList = users;
+      if (this.checkedOutList.includes(this.user)) {
+        this.isShow = true;
+      }
     });
   }
 }
