58d04ce28c2ef000d7d42746ff81e45a011fb6b3
unknown <aloha.hat.2508@gmail.com>
2021-01-21 12:16:48 +0700

    Lấy được thông tin trong rate form

--- src/app/modules/rating/rating-create/rating-create.component.ts
+++ src/app/modules/rating/rating-create/rating-create.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import {environment} from '../../../environments/environment';
 import {UserService} from '../../../service/user-service/user.service';
 import {FormBuilder, FormGroup} from '@angular/forms';
+import {Rating} from '../../../model/rating-model/rating';
+import {User} from '../../../model/user-model/user';
+import {House} from '../../../model/house-model/house';
+import {Timestamp} from 'rxjs';
 
 @Component({
   selector: 'app-rating-create',
@@ -13,10 +17,13 @@ export class RatingCreateComponent implements OnInit {
   user: any;
   notRatedBookingList: any;
   rateForm: FormGroup;
+  // isShow = true;
   constructor(private userService: UserService,
               private fb: FormBuilder) { }
   ngOnInit(): void {
     this.rateForm = this.fb.group({
+      userId: [''],
+      houseId: [''],
       rate: [''],
       review: ['']
     });
@@ -35,6 +42,25 @@ export class RatingCreateComponent implements OnInit {
   }
 
   createRate() {
-    console.log('kết quả rate' + this.rateForm.value.review, this.rateForm.value.rate);
+    const newRate = this.rateForm.value;
+    newRate.review = this.rateForm.value.review;
+    newRate.rate = this.rateForm.value.review;
+    newRate.userId = this.user;
+    newRate.houseId = this.rateForm.value.houseId;
+    console.log(newRate.review);
+    alert('Thank you for your feedback!');
+    // this.userService.getUserProfile(this.currentUser.username).subscribe(value => {
+    //   this.user = value;
+    //   if (this.user) {
+    //     this.userService.findNotRatedBookingByUser(this.user.userId).subscribe((data) => {
+    //       this.notRatedBookingList = data;
+    //       console.log('list chưa rate' + this.notRatedBookingList);
+    //     });
+    //   }
+    // });
   }
+
+  // hideRateForm() {
+  //   this.isShow = true;
+  // }
 }
