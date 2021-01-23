211adca45d945509f2d9fcf4a49dc4fab24955fe
unknown <aloha.hat.2508@gmail.com>
2021-01-23 22:05:07 +0700

    login thanh cong nhung khong logout duoc

--- src/app/service/authen-service/auth.service.ts
+++ src/app/service/authen-service/auth.service.ts
@@ -8,7 +8,9 @@ import {HttpClient} from '@angular/common/http';
 import {map} from 'rxjs/operators';
 import {User} from '../../model/user-model/user';
 import {GoogleToken} from '../../model/googleToken-model/GoogleToken';
+import {Router} from '@angular/router';
 const API_URL = `${environment.apiUrl}`;
+declare var gapi: any;
 @Injectable({
   providedIn: 'root'
 })
@@ -19,7 +21,8 @@ export class AuthService {
   private currentUserSubject: BehaviorSubject<UserToken>;
   public currentUser: Observable<UserToken>;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,
+              private router: Router) {
     // @ts-ignore
     this.currentUserSubject = new BehaviorSubject<UserToken>(JSON.parse(localStorage.getItem('user')));
     this.currentUser = this.currentUserSubject.asObservable();
@@ -28,7 +31,18 @@ export class AuthService {
   public get currentUserValue(): UserToken {
     return this.currentUserSubject.value;
   }
-
+  initGoogleOAuth(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      gapi.load('auth2', async () => {
+        const gAuth = await gapi.auth2.init({
+          client_id: '360187321088-pphmdafbluqev38ctadkfvj02v7n346p.apps.googleusercontent.com',
+          fetch_basic_profile: true,
+          scope: 'profile email'
+        });
+        resolve(gAuth);
+      }, reject);
+    });
+  }
   // tslint:disable-next-line:typedef
   login(inputUser: User) {
     return this.http.post(API_URL + '/login', inputUser)
@@ -42,9 +56,17 @@ export class AuthService {
   }
 
   // tslint:disable-next-line:typedef
-  logout() {
+  async logout() {
     localStorage.removeItem('user');
     // @ts-ignore
+    gapi = await this.initGoogleOAuth();
+    const auth2 = gapi.auth2.getAuthInstance();
+    console.log(auth2);
+    if (auth2) {
+      auth2.signOut().then(() => {
+        auth2.disconnect();
+      });
+    }
     this.currentUserSubject.next(null);
   }
 
@@ -55,8 +77,13 @@ export class AuthService {
   // tslint:disable-next-line:typedef
   googleSignIn(token: GoogleToken) {
     console.log(token);
-    return this.http.post<any>(API_URL + '/googleSignIn', token).subscribe(resp => {
-      console.log(resp);
+    return this.http.post<any>(API_URL + '/googleSignIn', token).subscribe(user => {
+      localStorage.setItem('user', JSON.stringify(user));
+      // @ts-ignore
+      this.currentUserSubject.next(user);
+      this.update.emit('login');
+      console.log(user);
+      this.router.navigate(['/']);
     });
   }
 }
